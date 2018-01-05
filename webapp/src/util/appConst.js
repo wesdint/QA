37b4dd192bae6ba818c9139a20c44b914c286480
wesdint <zengwendong1010@gmail.com>
2018-01-05 17:22:30 +0800

    改变主域

--- webapp/src/util/appConst.js
+++ webapp/src/util/appConst.js
@@ -2,7 +2,7 @@
 let BACKEND_DOMAIN = 'http://localhost:3000'
 if (process.env.NODE_ENV === 'production') {
   // 生产环境
-  BACKEND_DOMAIN = 'http://node.bluemoon.com.cn'
+  BACKEND_DOMAIN = 'http://182.61.10.70:3000'
 } else if (process.env.NODE_ENV === 'development') {
   // 本地开发
   BACKEND_DOMAIN = 'http://localhost:3000'
